01f5f241574b467fd20647182ea637f77afe80af
user <user@USER->
2017-11-23 10:34:29 +1000

    commit4

--- Проект1/Проект1/Source.cpp
+++ Проект1/Проект1/Source.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 	string name;
-	cout << "Name: Dmitry, Roman" << endl;
+	cout << "Name: Dmitry, Roman, Kol9, Vlad" << endl;
 	
 	cout << "Input name" << endl;
 	cin >> name;
 	if (name == "Dmitry") cout << "Hello Dmitry" << endl;
 	else if (name == "Roman") cout << "Hello Roman" << endl;
 	else if (name == "Kol9") cout << "Hello Kol9" << endl;
-
+	else if (name == "Vlad") cout << "Hello Vlad" << endl;
 	else cout << "Hello User" << endl;
 
 	system("pause"); 
