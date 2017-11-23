de30ea62e99f24dedd551905e057bd62482aba6f
user <user@USER->
2017-11-23 10:26:37 +1000

    commit2

--- Проект1/Проект1/Source.cpp
+++ Проект1/Проект1/Source.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Hello, world!" << endl;
+	string name;
+	cout << "Name: Dmitry, Roman" << endl;
+	
+	cout << "Input name" << endl;
+	cin >> name;
+	if (name == "Dmitry") cout << "Hello Dmitry" << endl;
+	else if (name == "Roman") cout << "Hello Roman" << endl;
+	else cout << "Hello User" << endl;
+
 	system("pause"); 
 	return 0;
 }
